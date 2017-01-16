52e95121931d45fb4d0761b52595fc63d940344e
liloman <eselilo@gmail.com>
2017-01-16 09:38:46 +0100

    linux mint errors ================== 1-all glade objects need an id 2-not standar dbus address

--- pomodoro-client.py
+++ pomodoro-client.py
@@ -5,6 +5,7 @@ import getopt
 import dbus
 import sys
 import os
+import re
 
 def usage():
     print """
@@ -46,7 +47,53 @@ if len(sys.argv) == 1:
 
 com=sys.argv[1]
 
-dbus_path="unix:path=/run/user/"+str(os.getuid())+"/bus"
+#Get address bus
+if os.path.exists("/run/user/"+str(os.getuid())+"/bus"):
+    dbus_path="unix:path=/run/user/"+str(os.getuid())+"/bus"
+elif os.getenv('DBUS_SESSION_BUS_ADDRESS'):
+    print str(os.getenv(DBUS_SESSION_BUS_ADDRESS))
+    dbus_path=str(os.getenv(DBUS_SESSION_BUS_ADDRESS))
+else:
+    # We need to get the dbus environment variable 
+    # These environment variables are stored in $HOME/.dbus/session-bus
+    # there may be old sessions stored in .dbus/session-bus, we need to determine
+    # which session to use
+
+    env = os.environ.copy()
+    # Fetch all sessions in $HOME/.dbus/session-bus
+    dbus_dir=os.path.join(env['HOME'],'.dbus/session-bus')
+    dbus_sessions=[]
+    for filename in os.listdir(dbus_dir):
+        filepath = os.path.join(dbus_dir,filename)
+        session=dict()
+        with open(filepath, 'r') as f:
+            for line in f.readlines():
+                if re.match('^#',line):
+                    continue
+                var,value=line.strip().split('=',1)
+                session[var]=value
+        dbus_sessions.append(session)
+
+    # Determine which dbus session to use
+    # Check pid to see if it is alive
+    # If pid is alive see if the name contains dbus-daemon
+    for s in dbus_sessions:
+        pid=s['DBUS_SESSION_BUS_PID']
+
+        if os.path.exists('/proc/{0}'.format(pid)):
+            with open('/proc/{0}/cmdline'.format(pid)) as f:
+                cmdline = f.readline().strip()
+        else:
+            # DBUS process must be dead
+            continue
+
+        if re.search('dbus-daemon',cmdline):
+            dbus_path=s['DBUS_SESSION_BUS_ADDRESS']
+            break
+
+if not dbus_path:
+    print "Couldn't find a dbus session address"
+    sys.exit(1)
 
 try:
     # use busConnection better than bus = dbus.SessionBus() to work with systemd for example
